f6576798b0bd5399404611ec245a0d99ddfee68b
AlexandreMeirinhosShark <shark05980@sharkcoders.pt>
2024-10-04 19:11:27 +0100

    Add files via upload

---
+++ aula2.py
@@ -0,0 +1,8 @@
+# Entradas
+nome = input("Nome: ")
+pelido = input("Apelido: ")
+curso = input("Curso: ")
+
+# Escreve aqui a frase.
+print(f"Boas vindas, {nome} {pelido}. És um aluno do curso {curso}.")
+#
